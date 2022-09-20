15813fe86ce8d33f94966b8fcaf52d68812f25ca
mohamed930 <mohammedali12477@gmail.com>
2022-09-20 20:12:05 +0200

    initialize application as MVC Design pattern commit

---
+++ card-generator/AppDelegate/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  card-generator
+//
+//  Created by Mohamed Ali on 20/09/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
